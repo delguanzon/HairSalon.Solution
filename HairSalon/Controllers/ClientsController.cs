89ab0c23410abda887d7ecdb254ed154986464ad
delguanzon <yodelguanzon@gmail.com>
2023-01-02 04:39:52 -0800

    Add clients MVC

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -1,15 +1,78 @@
 using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
-    public class ClientController : Controller
+  public class ClientsController : Controller
+  {
+
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db) 
     {
-
-      [HttpGet("/clients")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    public ActionResult Index()
+    {
+    List<Client> model = _db.Clients.ToList();
+    ViewBag.PageTitle = "View All Stylist";
+    return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients
+                        .Include(client => client.StylistId)
+                        .FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+        return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+        _db.Clients.Update(client);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+        return View(thisClient);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+        _db.Clients.Remove(thisClient);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
+  }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -8,6 +8,7 @@ namespace HairSalon.Models
         public int StylistId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string MiddleName { get; set; }
         public string ContactNumber { get; set; }
         public string Status { get; set; }
         public DateTime DateCreated { get; }
